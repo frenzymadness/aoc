1ddd1f2657d63486199138e6cc08bfe6760f8018
Lumir Balhar <lbalhar@redhat.com>
2022-12-04 09:20:58 +0100

    2022 Day 4

---
+++ 2022/04/1.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+import re
+
+here = Path(__file__).parent
+
+pairs = 0
+
+with open(here / "input") as input:
+    for line in input:
+        l1, u1, l2, u2 = re.match(r"(\d+)-(\d+),(\d+)-(\d+)", line.strip()).groups()
+        elf1 = set(range(int(l1), int(u1) + 1))
+        elf2 = set(range(int(l2), int(u2) + 1))
+
+        if elf1.issubset(elf2) or elf1.issuperset(elf2):
+            pairs += 1
+
+print(pairs)
